99340eed3b4e58d05b9911d126dc804eb3ca2af6
adamulrich <adamulrich@hotmail.com>
2023-04-16 18:19:09 -0700

    added units

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -22,7 +22,7 @@ namespace MyFirstConsoleApplication
 
             // print out days until christmas
             var christmasDay = new DateTime(date.Year, 12, 25);
-            var daysUntilChristmas = christmasDay -  date;
+            var daysUntilChristmas = christmasDay - date;
             Console.WriteLine($"There are {daysUntilChristmas.Days} days until Christmas");
 
             //add code from 2.1 example
@@ -35,14 +35,14 @@ namespace MyFirstConsoleApplication
             do
             {
                 Console.Write("Give the width of the window between " +
-                MIN_WIDTH + " and " + MAX_WIDTH + " :");
+                MIN_WIDTH + " and " + MAX_WIDTH + " in meters :");
                 widthString = Console.ReadLine();
                 width = double.Parse(widthString);
             } while (width < MIN_WIDTH || width > MAX_WIDTH);
             do
             {
                 Console.Write("Give the height of the window between " +
-                MIN_HEIGHT + " and " + MAX_HEIGHT + " :");
+                MIN_HEIGHT + " and " + MAX_HEIGHT + " in meters:");
                 heightString = Console.ReadLine();
                 height = double.Parse(heightString);
             } while (height < MIN_HEIGHT || height > MAX_HEIGHT);
@@ -51,8 +51,7 @@ namespace MyFirstConsoleApplication
             Console.WriteLine("The length of the wood is " +
             woodLength + " feet");
             Console.WriteLine("The area of the glass is " +
-            glassArea + " square metres");
-
+            glassArea + " square meters");
 
             // add a press any key to continue
             Console.WriteLine("Press any key to continue....");
